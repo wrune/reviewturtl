afabb67d3ae60961d1c2786302da77cba6ffdec3
RheagalFire <arishalam121@gmail.com>
2024-07-28 01:50:20 +0530

    clean later

--- reviewturtl/src/optimizers/base_optimizer.py
+++ reviewturtl/src/optimizers/base_optimizer.py
@@ -1,15 +1,45 @@
+import pandas as pd
+import dspy
+
+
 class TurtlBaseOptimizer:
     def __init__(self, optimizer_name: str):
         self.optimizer_name = optimizer_name
 
-    def load_prompt_model(self):
-        pass
+    def load_prompt_model(self, model):
+        self.prompt_model = model
+        return self.prompt_model
 
-    def load_task_model(self):
-        pass
+    def load_task_model(self, model):
+        self.task_model = model
+        return self.task_model
 
-    def load_training_data(self):
-        pass
+    def load_trainset(
+        self,
+        csv_path: str,
+        use_columns: list[str],
+        input_columns: list[str],
+        output_columns: list[str],
+    ):
+        # by default, read all columns
+        if use_columns is None:
+            trainset_csv = pd.read_csv(csv_path)
+            use_columns = trainset_csv.columns
+        else:
+            # if use columns is specified, read only those columns
+            trainset_csv = pd.read_csv(csv_path, usecols=use_columns)
+        # Convert the dataframe to a list of dspy.Example objects
+        trainset = [
+            # Dspy Example object
+            dspy.Example(**{col: row[col] for col in use_columns}).with_inputs(
+                *input_columns
+            )
+            for _, row in trainset_csv.iterrows()
+        ]
+        return trainset
+
+    def load_optimizer(self):
+        raise NotImplementedError("Subclass must implement this method")
 
     def generate_training_data(self):
         pass
@@ -18,4 +48,4 @@ class TurtlBaseOptimizer:
         pass
 
     def optimize(self):
-        pass
+        raise NotImplementedError("Subclass must implement this method")
