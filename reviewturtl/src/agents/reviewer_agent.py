d2dde75562f1f28a15a80c4be207193d5b8cdb16
Aarish Alam <arishalam121@gmail.com>
2024-08-14 20:31:13 -0400

    add file content to signature (#21)

--- reviewturtl/src/agents/reviewer_agent.py
+++ reviewturtl/src/agents/reviewer_agent.py
@@ -1,4 +1,6 @@
-from reviewturtl.src.programmes.programmes import TypedChainOfThoughtProgramme as DspyProgramme
+from reviewturtl.src.programmes.programmes import (
+    TypedChainOfThoughtProgramme as DspyProgramme,
+)
 from reviewturtl.src.signatures.signatures import ReviewerSignature
 from reviewturtl.src.agents.base_agent import Agent
 
@@ -7,12 +9,14 @@ class ReviewerAgent(Agent):
     def __init__(self):
         super().__init__(DspyProgramme(signature=ReviewerSignature))
 
-    def forward(self, file_diff,model=None):
-        self.prediction_object = self.programme.forward(file_diff=file_diff,model=model)
+    def forward(self, file_diff, file_content, model=None):
+        self.prediction_object = self.programme.forward(
+            file_diff=file_diff, file_content=file_content, model=model
+        )
         return self.prediction_object
-    
-    def __call__(self, file_diff,model=None):
-        return self.forward(file_diff,model=model)
+
+    def __call__(self, file_diff, file_content, model=None):
+        return self.forward(file_diff, file_content, model=model)
 
 
 __all__ = ["ReviewerAgent"]
