f4da79e775733d6158f08836e963b9019a0a9e2c
RheagalFire <arishalam121@gmail.com>
2024-08-25 17:56:58 +0530

    indexing methods

--- reviewturtl/src/code_search/indexer.py
+++ reviewturtl/src/code_search/indexer.py
@@ -18,6 +18,7 @@ class TurtlIndexer(TurtlPreprocessor):
     ):
         super().__init__(methods_dump_path, text_embedding_model, code_embedding_model)
         self.qdrant_client = qdrant_client
+        self.db_functions = None
 
     def preprocess_methods_for_indexing(self, methods):
         """
@@ -33,25 +34,15 @@ class TurtlIndexer(TurtlPreprocessor):
         code_embeddings = self.get_code_embeddings(code_representations)
         return text_embeddings, code_embeddings
 
-    def index(
-        self,
-        collection_name: str,
-        mode: Optional[str] = None,
-        file_metadata: Optional[Any] = None,
-    ):
+    def method_indexing_pipeline(self, methods, collection_name):
         """
-        Index documents in Qdrant
+        Method indexing pipeline that computes embeddings and uploads to Qdrant
         """
-        if mode is None:
-            # get methods from the specified path
-            methods = self.get_methods(self.methods_dump_path)
-        elif mode == IndexingMode.PARSE_FROM_FILE.value:
-            file_metadata = [meta.model_dump() for meta in file_metadata]
-            methods = self.get_methods_from_file_content(file_metadata)
         # preprocess methods for indexing
         text_embeddings, code_embeddings = self.preprocess_methods_for_indexing(methods)
         # Check if collection exists
         if not self.qdrant_client.collection_exists(collection_name):
+            # create the collection if it doesnot exist already
             self.qdrant_client.create_collection(
                 collection_name,
                 vectors_config={
@@ -87,3 +78,51 @@ class TurtlIndexer(TurtlPreprocessor):
         except Exception as e:
             logger.error(f"Error uploading points to Qdrant: {e}")
             return False
+
+    def index(
+        self,
+        collection_name: str,
+        mode: Optional[str] = None,
+        file_metadata: Optional[Any] = None,
+    ):
+        """
+        Index documents in Qdrant for addition to the index
+        Args:
+            collection_name: str
+            mode: str
+            file_metadata: List[Dict[str, Any]]
+        Returns:
+            bool
+        """
+        if mode is None:
+            # get methods from the specified path
+            methods = self.get_methods(self.methods_dump_path)
+        elif mode == IndexingMode.PARSE_FROM_FILE.value:
+            file_metadata = [meta.model_dump() for meta in file_metadata]
+            methods = self.get_methods_from_file_content(file_metadata)
+
+        return self.method_indexing_pipeline(methods, collection_name)
+
+    def update_index(self, collection_name: str, file_metadata: Optional[Any] = None):
+        """
+        Update the index for files that are edidted
+        Args:
+            collection_name: str
+            file_metadata: List[Dict[str, Any]]
+        Returns:
+            bool
+        """
+        file_metadata = [meta.model_dump() for meta in file_metadata]
+        # get the id's of the files from primary database
+        file_ids = self.db_functions.get_file_ids(file_metadata)
+        # delete these points from qdrant
+        try:
+            self.qdrant_client.delete_points(
+                collection_name, points_selector=models.PointIdsList(points=file_ids)
+            )
+        except Exception as e:
+            logger.error(f"Error deleting points from Qdrant: {e}")
+            return False
+        # get methods from the file content once the points are deleted
+        methods = self.get_methods_from_file_content(file_metadata)
+        return self.method_indexing_pipeline(methods, collection_name)
