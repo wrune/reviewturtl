a4db6f4a58d10a96770abca2eb2ac829ee80dd41
Aarish Alam <arishalam121@gmail.com>
2024-09-14 12:04:58 -0400

    Add scrutnizer and handlers to the Webhook Endpoints (#42)
    * add scrutnizer and handlers
    
    * remove comment_id
    
    * change to debug statements
    
    * test
    
    * temp_changes
    
    * Revert "temp_changes"
    
    This reverts commit cb9f57e84c1bcb833bf9ea702ad046a390464852.
    
    * test turtl

--- reviewturtl/api/routers/events_webhook.py
+++ reviewturtl/api/routers/events_webhook.py
@@ -1,20 +1,9 @@
 from fastapi import APIRouter, Request, HTTPException, status
 from reviewturtl.logger import get_logger
-from reviewturtl.settings import get_settings
-from reviewturtl.clients.github_client import (
-    fetch_diff_content,
-    post_github_comment,
-    update_github_comment,
-    get_existing_comment,
-)
-from reviewturtl.clients.ai_client import call_summarizer
-import json
+from reviewturtl.src.event_utils.pr_scrutnizer import pr_action_handler
 
-settings = get_settings()
 log = get_logger(__name__)
 router = APIRouter()
-
-COMMENT_IDENTIFIER = "AI Summary of Changes"
 
 
 @router.post("/api/v1/github_webhook", status_code=status.HTTP_204_NO_CONTENT)
@@ -30,7 +19,7 @@ async def github_webhook(request: Request):
         form_data = await request.form()
         payload = {key: value for key, value in form_data.items()}
     else:
-        log.error(f"Invalid content-type: {content_type}")
+        log.error(f"Invalid content-type: {content_type}", exc_info=True)
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Invalid payload: content-type must be application/json or application/x-www-form-urlencoded, got {content_type}",
@@ -40,65 +29,7 @@ async def github_webhook(request: Request):
     log.debug(f"Received event: {event}")
 
     if event == "pull_request":
-        # Log the payload
-        log.debug(f"Payload: {payload}")
-        payload = json.loads(payload.get("payload", {}))
-        action = payload.get("action")
-        pr_number = payload.get("number")
-        pr_title = payload.get("pull_request", {}).get("title")
-        repo_full_name = payload.get("repository", {}).get("full_name")
-        request_id = request.headers.get("X-Request-ID", "default-request-id")
-        diff_url = payload.get("pull_request", {}).get("diff_url")
-        body = payload.get("pull_request", {}).get("body")
-        github_token = settings.PAT_TOKEN
-        owner, repo = repo_full_name.split("/")
-        log.debug(f"Diff URL: {diff_url}")
-        log.debug(f"Body: {body}")
-
-        # Fetch diff content
-        file_diff = await fetch_diff_content(owner, repo, pr_number, github_token)
-        log.debug(f"File Diff Content: {file_diff}")
-
-        # Call summarizer endpoint
-        summary_response = await call_summarizer(file_diff, request_id)
-        summary = summary_response.get("data", {}).get(
-            "walkthrough", "No summary available"
-        )
-        tabular_summary = summary_response.get("data", {}).get(
-            "tabular_summary", "No tabular summary available"
-        )
-
-        comment_body = f"### {COMMENT_IDENTIFIER}\n\n{summary}\n\n**Tabular Summary:**\n\n{tabular_summary}"
-
-        if action == "opened":
-            # Post new comment
-            comment = await post_github_comment(
-                pr_number, comment_body, owner, repo, github_token
-            )
-            log.info(f"Posted new comment to PR #{pr_number}: {comment['id']}")
-        elif action in ["synchronize", "edited"]:
-            # Fetch existing comment
-            existing_comment = await get_existing_comment(
-                pr_number, owner, repo, github_token, identifier=COMMENT_IDENTIFIER
-            )
-            if existing_comment:
-                # Update existing comment, if identifier is present with identifier
-                updated_comment = await update_github_comment(
-                    existing_comment["id"], comment_body, owner, repo, github_token
-                )
-                log.info(
-                    f"Updated comment {existing_comment['id']} on PR #{pr_number} with {updated_comment['body']}"
-                )
-            else:
-                # If no existing comment is found with the identifier , post a new one
-                comment = await post_github_comment(
-                    pr_number, comment_body, owner, repo, github_token
-                )
-                log.info(f"Posted new comment to PR #{pr_number}")
-        else:
-            log.warning(f"Unhandled pull_request action: {action}")
-
-        log.info(f"Pull Request #{pr_number} {action}: {pr_title}")
+        await pr_action_handler(payload, request)
     elif event == "push":
         # Handle push event
         log.info(f"Push event received with payload: {payload}")
